9e96e0fe50acdc6a126c1d40d2dfb60a960b437e
R Searls <rsearls@redhat.com>
2021-11-15 10:09:50 -0500

    made minor transformer rules change to core module

--- core/src/main/java/org/apache/cxf/common/jaxb/JAXBContextCache.java
+++ core/src/main/java/org/apache/cxf/common/jaxb/JAXBContextCache.java
@@ -194,7 +194,7 @@ public final class JAXBContextCache {
             if (HAS_MOXY) {
                 map.put("eclipselink.default-target-namespace", defaultNs);
             }
-            map.put("org.glassfish.jaxb.defaultNamespaceRemap", defaultNs);
+            map.put("com.sun.xml.bind.defaultNamespaceRemap", defaultNs);
         }
         if (props != null) {
             map.putAll(props);
